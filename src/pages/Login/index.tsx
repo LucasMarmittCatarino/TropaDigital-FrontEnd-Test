160143a11104a09abe5e49ee5e33159d32a79ab2
Lucas Marmitt Catarino <lucasma@ps.grupopanvel.com.br>
2025-06-07 19:21:33 -0300

    finished LoginScreen

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -25,17 +25,17 @@ function Login() {
     };
 
   return (
-    <div className="flex justify-center items-center h-screen w-screen p-[15px] bg-[#F9FBFF]">
+    <div className="flex-container h-screen p-[15px] bg-[var(--background)]">
 
-      <div className="flex items-center justify-center relative max-w-[756px] w-full min-h-[498px] bg-white p-[10px] rounded-[20px] shadow-2xl">
+      <div className="flex-container relative max-w-[756px]  min-h-[498px] bg-[var(--white)] p-[10px] rounded-[20px] shadow-2xl">
 
-        <div className='flex flex-col w-full min-h-[388px] ml-[20px] mr-[42px] max-[611px]:mr-[20px]'>
+        <div className='flex-column w-full min-h-[388px] ml-[20px] mr-[42px] max-[611px]:mr-[20px]'>
             <img src={Logo} alt="Logo da empresa" className="max-w-[155px] h-[25px] mb-[35px]"/>
-            <h1 className="font-bold text-[26px] text-[#CC6237] leading-[15px] max-[361px]:text-[23px]">Bem-vindo de volta</h1>
-            <p className="text-[13px] font-normal text-[#2A4D8E80] max-[361px]:text-[10px]">Entre com sua conta para acessar o painel.</p>
+            <h1 className="text-[26px] font-bold text-[var(--main-orange)] leading-[20px] max-[361px]:text-[23px]">Bem-vindo de volta</h1>
+            <p className="text-[13px] text-[var(--grey-1)] max-[361px]:text-[10px]">Entre com sua conta para acessar o painel.</p>
 
-            <form onSubmit={handleLogin} className="flex flex-col gap-[20px] mt-[35px]">
-                <div className="flex flex-col">
+            <form onSubmit={handleLogin} className="flex-column gap-[20px] mt-[35px]">
+                <div className="flex-column">
                 <label htmlFor="email" className="label-default">E-mail</label>
                 <input
                     type="email"
@@ -45,17 +45,14 @@ function Login() {
                 />
                 </div>
 
-                <div className="flex flex-col">
-                    <label htmlFor="password" className="label-default">
-                        Senha
-                    </label>
-
+                <div className="flex-column">
+                    <label htmlFor="password" className="label-default">Senha</label>
                     <div className="relative">
                         <input
                         type={showPassword ? "text" : "password"}
                         id="password"
                         placeholder="Digite aqui"
-                        className="input-default pr-10"
+                        className="input-default"
                         />
                         {showPassword ? (
                         <MdOutlineVisibilityOff
@@ -73,14 +70,14 @@ function Login() {
                     </div>
                 </div>
 
-                <button type="submit" className="w-full h-[40px] bg-[#CC6237] rounded-[100px] cursor-pointer hover:bg-[#b4542e] transition-colors duration-300">
-                  <p className='text-white font-medium text-[13px]'>Enviar</p>
+                <button type="submit" className="w-full h-[40px] bg-[var(--main-orange)] rounded-[100px] cursor-pointer hover:bg-[var(--main-orange-dark)] transition-colors duration-300">
+                  <p className='text-[var(--white)] font-medium text-[13px]'>Enviar</p>
                 </button>
             </form>
         </div>
 
         {isWideEnough && (
-          <div className="flex items-end ml-auto max-w-[355px] w-full min-h-[478px] rounded-[20px] bg-[#CC6237]">
+          <div className="flex items-end ml-auto max-w-[355px] w-full min-h-[478px] rounded-[20px] bg-[var(--main-orange)]">
             <img
               src={LoginImage}
               alt="Imagem Ilustrativa"
