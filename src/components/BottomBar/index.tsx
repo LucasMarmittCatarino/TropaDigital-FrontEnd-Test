3c2288fdfdea52ed01c92ba7f9bc75df9186b67e
Lucas Marmitt Catarino <lucasma@ps.grupopanvel.com.br>
2025-06-07 22:48:56 -0300

    feat: added bottom navigator for mobile

---
+++ src/components/BottomBar/index.tsx
@@ -0,0 +1,50 @@
+import { NavLink } from "react-router-dom";
+import { RxDashboard } from "react-icons/rx";
+import { LuCalendarDays } from "react-icons/lu";
+import { HiOutlineUserGroup } from "react-icons/hi2";
+import { RiGroupLine } from "react-icons/ri";
+import { IoPersonOutline } from "react-icons/io5";
+import { FaPowerOff } from "react-icons/fa6";
+
+function BottomBar() {
+  const navItemClass = ({ isActive }: { isActive: boolean }) =>
+    `flex flex-col items-center justify-center px-3 py-2 rounded ${
+      isActive ? "checked-navbar text-[#CC6237]" : "hover-navbar text-black"
+    }`;
+
+  return (
+    <nav className="fixed bottom-0 left-0 w-full h-[60px] bg-white border-t border-black/10 flex items-center justify-between px-4 z-50">
+      {/* Grupo 1 - Ícones principais com espaçamento total */}
+      <div className="flex flex-1 justify-between">
+        <NavLink to="/Dashboard" className={navItemClass}>
+          <RxDashboard size={22} />
+        </NavLink>
+        <NavLink to="/Events" className={navItemClass}>
+          <LuCalendarDays size={22} />
+        </NavLink>
+        <NavLink to="/Teams" className={navItemClass}>
+          <HiOutlineUserGroup size={22} />
+        </NavLink>
+        <NavLink to="/Registration" className={navItemClass}>
+          <RiGroupLine size={22} />
+        </NavLink>
+      </div>
+
+      {/* Grupo 2 - Linha + ícones de usuário */}
+      <div className="flex items-center ml-[10px]">
+        {/* Linha separadora */}
+        <div className="w-[1px] h-[34px] bg-black/10 mr-[10px]" />
+
+        {/* Botões do usuário */}
+        <NavLink to="/UpdateProfile" className={navItemClass}>
+          <IoPersonOutline size={22} />
+        </NavLink>
+        <NavLink to="/" className={navItemClass}>
+          <FaPowerOff size={22} />
+        </NavLink>
+      </div>
+    </nav>
+  );
+}
+
+export default BottomBar;
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,13 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import BottomBar from "../components/BottomBar";
 
 export default function MainLayout() {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <main className="flex-1 bg-gray-100 overflow-y-auto">
+    <div className="flex flex-col h-screen">
+      
+      <div className="hidden min-[560px]:flex">
+        <Sidebar />
+        <main className="flex-1 bg-gray-100 overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
+
+      <div className="flex-1 max-[560px]:flex hidden flex-col bg-gray-100 overflow-y-auto">
         <Outlet />
-      </main>
+        <BottomBar />
+      </div>
     </div>
   );
 }
