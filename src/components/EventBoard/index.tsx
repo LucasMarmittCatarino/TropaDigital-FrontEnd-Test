b7423cb6dab37c3b74ee0904d3206271f7c5165b
Lucas Marmitt Catarino <lucasma@ps.grupopanvel.com.br>
2025-06-07 22:29:13 -0300

    fix: updated dashboard responsivity

--- src/components/EventBoard/index.tsx
+++ src/components/EventBoard/index.tsx
@@ -3,13 +3,14 @@ import { CiSearch } from "react-icons/ci";
 import { FiPlus } from "react-icons/fi";
 import { dashboard_data } from '../../mocks/dashboard-data';
 import { EventDashboardTable } from '../EventDashBoardTable';
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
 function EventBoard () {
     return (
         <div className="flex flex-col w-full bg-[#FFFFFF] border border-black/10 rounded-[10px] p-[20px]">
 
-            <div className='flex gap-[10px] ml-auto mb-[12px]'>
-                <div className="flex items-center gap-[15px] w-[203px] h-[36px] bg-[#F6F6F6] rounded-[33px] px-[10px]">
+            <div className='flex w-full justify-end gap-[10px] mb-[12px]'>
+                <div className="flex items-center gap-[15px] max-w-[203px] w-full h-[36px] bg-[#F6F6F6] rounded-[33px] px-[10px]">
                     <CiSearch size={16} />
                     <input
                         type="search"
@@ -18,35 +19,40 @@ function EventBoard () {
                     />
                 </div>
 
-                <button className="flex w-[124px] h-[36px] items-center px-[15px] gap-[5px] bg-[#CC6237] text-white rounded-[33px] hover:bg-[#b6582f] transition">
+                <button className="flex items-center justify-center bg-[#CC6237] text-white rounded-[33px] hover:bg-[#b6582f] transition-colors duration-200 cursor-pointer
+                    w-[124px] h-[36px] px-[15px] gap-[5px]
+                    max-[815px]:w-[35px] max-[815px]:h-[35px] max-[815px]:px-0 max-[815px]:gap-0 max-[815px]:rounded-full"
+                >
                     <FiPlus size={16} />
-                    <span className="text-[13px] font-medium cursor-pointer">Inserir novo</span>
+                    <span className="text-[13px] font-medium max-[815px]:hidden">Inserir novo</span>
                 </button>
+
             </div>
 
             <EventDashboardTable dashboard_data={dashboard_data}/>
 
-            <div className='flex gap-[10px] mt-[29px] ml-auto'>
-                <button
-                className="w-[75px] h-[35px] bg-[#F5F5F5] rounded-[200px] hover:bg-[#d47a3c] transition flex items-center justify-center cursor-pointer"
-                >
-                <span className="text-[12px] font-normal text-black">Anterior</span>
+            <div className="flex gap-[10px] mt-[29px] ml-auto">
+
+                <button className="flex items-center justify-center bg-[#F5F5F5] rounded-full hover:bg-[#d47a3c] transition-colors duration-200 cursor-pointer w-[77px] h-[35px] max-[815px]:w-[35px] max-[815px]:h-[35px]">
+                    <span className="text-[12px] font-normal text-black max-[815px]:hidden">Anterior</span>
+                    <FaChevronLeft size={12} className="hidden max-[815px]:block text-black" />
                 </button>
 
-                <div className='flex items-center justify-center w-[35px] h-[35px] bg-[#CC6237] rounded-[200px] hover:bg-[#b6582f] transition-colors duration-200 cursor-pointer'>
-                    <p className='text-white font-normal text-[12px]'>1</p>
-                </div>
-                    <div className='flex items-center justify-center w-[35px] h-[35px] bg-[#F5F5F5] rounded-[200px] hover:bg-[#d47a3c] transition-colors duration-200 cursor-pointer'>
-                    <p className='text-black font-normal text-[12px]'>2</p>
-                </div>
-                    <div className='flex items-center justify-center w-[35px] h-[35px] bg-[#F5F5F5] rounded-[200px] hover:bg-[#d47a3c] transition-colors duration-200 cursor-pointer'>
-                    <p className='text-black font-normal text-[12px]'>3</p>
-                </div>
+                <button className="flex items-center justify-center w-[35px] h-[35px] bg-[#CC6237] rounded-full hover:bg-[#b6582f] transition-colors duration-200 cursor-pointer">
+                    <p className="text-white font-normal text-[12px]">1</p>
+                </button>
 
-                <button
-                    className="w-[77px] h-[35px] bg-[#CC6237] rounded-[200px] hover:bg-[#b6582f] transition flex items-center justify-center cursor-pointer"
-                    >
-                    <span className="text-[12px] font-normal text-white">Próxima</span>
+                <button className="flex items-center justify-center w-[35px] h-[35px] bg-[#F5F5F5] rounded-full hover:bg-[#d47a3c] transition-colors duration-200 cursor-pointer">
+                    <p className="text-black font-normal text-[12px]">2</p>
+                </button>
+
+                <button className="flex items-center justify-center w-[35px] h-[35px] bg-[#F5F5F5] rounded-full hover:bg-[#d47a3c] transition-colors duration-200 cursor-pointer">
+                    <p className="text-black font-normal text-[12px]">3</p>
+                </button>
+
+                <button className="flex items-center justify-center bg-[#CC6237] rounded-full hover:bg-[#b6582f] transition-colors duration-200 cursor-pointer w-[77px] h-[35px] max-[815px]:w-[35px] max-[815px]:h-[35px]">
+                    <span className="text-[12px] font-normal text-white max-[815px]:hidden">Próxima</span>
+                    <FaChevronRight size={12} className="hidden max-[815px]:block text-white" />
                 </button>
             </div>
 
